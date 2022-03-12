4622dae76cd17f222e252378330d90ec6c1c4b51
Emil Ksenzovsky <a>
2022-03-12 17:33:19 +0200

    remove fastapi_dependency_registry.py with its tests

--- fastapidi/dependency_registry/__init__.py
+++ fastapidi/dependency_registry/__init__.py
@@ -1,2 +1,1 @@
 from .generic_dependency_registry import GenericDependencyRegistry
-from .fastapi_dependency_registry import FastApiDependencyRegistry
--- tests/dependency_registry/test_fastapi_dependency_registry.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-class TestFastApiDependencyRegistry:
-    @pytest.mark.skip(reason='not sure how to test it, it has only one line of private attribute use')
-    def test_to_dependency_overrides(self):
-        assert False
