9ac9ac84d0935f6d7e960625c48fde2beb8e431a
Emil Ksenzovsky <a>
2022-03-12 08:33:15 +0200

    remove setup.py file

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='FastApiDI',
-      version='0.1.1',
-      description='Dependency Injection Utility for FastApi',
-      author='Emil Ksenzovsky',
-      author_email='e.ksenzovski@gmail.com',
-      packages=find_packages()
-      )
