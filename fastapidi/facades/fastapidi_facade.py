310f3e612e01862a847b539ad39f3460fca033c8
emil <e.ksenzovski@gmail.com>
2022-04-23 09:36:35 +0300

    change FastApiDI class to also register dependencies as parametrized if they are

--- fastapidi/facades/fastapidi_facade.py
+++ fastapidi/facades/fastapidi_facade.py
@@ -1,10 +1,12 @@
-from typing import Dict
+from typing import Dict, Type, Any
 
 from fastapi import FastAPI
 
 from fastapidi.dependency_registry import GenericDependencyRegistry
+from fastapidi.dependency_registry.abc import TDependency, TImplementation, TImplementationFactory
 from fastapidi.factory_builders import FactoryBuilder
 from fastapidi.factory_builders.abc import AbstractFactoryBuilder
+from fastapidi.utils.dependencies import callable_has_params, create_parameterless_proxy_class
 
 
 class FastApiDI(GenericDependencyRegistry):
@@ -12,6 +14,27 @@ class FastApiDI(GenericDependencyRegistry):
         factory_builder: AbstractFactoryBuilder = FactoryBuilder()
         super().__init__(factory_builder)
         app.router.dependency_overrides_provider = self
+
+    def register_singleton(self, dependency_type: Type[TDependency], implementation_type: Type[TImplementation],
+                           *args: Any, **kwargs: Any) -> None:
+        super().register_singleton(dependency_type, implementation_type, *args, **kwargs)
+        if callable_has_params(dependency_type):
+            parameterless_dependency_type = create_parameterless_proxy_class(dependency_type)
+            super().register_singleton(parameterless_dependency_type, implementation_type, *args, **kwargs)
+
+    def register_scoped(self, dependency_type: Type[TDependency], implementation_type: Type[TImplementation],
+                        *args: Any, **kwargs: Any) -> None:
+        super().register_scoped(dependency_type, implementation_type, *args, **kwargs)
+        if callable_has_params(dependency_type):
+            parameterless_dependency_type = create_parameterless_proxy_class(dependency_type)
+            super().register_scoped(parameterless_dependency_type, implementation_type, *args, **kwargs)
+
+    def register_factory(self, dependency_type: Type[TDependency],
+                         implementation_factory: TImplementationFactory) -> None:
+        super().register_factory(dependency_type, implementation_factory)
+        if callable_has_params(dependency_type):
+            parameterless_dependency_type = create_parameterless_proxy_class(dependency_type)
+            super().register_factory(parameterless_dependency_type, implementation_factory)
 
     @property
     def dependency_overrides(self) -> Dict:
